0738b72487b62ff6cc4abc4423e5ffaade5e5084
Dimas-karas <dm2001@live.ru>
2026-03-08 13:06:40 +0300

    add HouseTensorDTO and update house_hypergraph to use it

---
+++ house_graph/__init__.py
@@ -0,0 +1,12 @@
+from .house_hypergraph import House, Node, Edge, HouseFactory, NODE_TYPES, EDGE_TYPES
+from .dto import HouseTensorDTO
+
+__all__ = [
+    "House",
+    "Node", 
+    "Edge",
+    "HouseFactory",
+    "NODE_TYPES",
+    "EDGE_TYPES",
+    "HouseTensorDTO",
+]
