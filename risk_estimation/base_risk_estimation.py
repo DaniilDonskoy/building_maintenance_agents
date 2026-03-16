fdfec126bd55d1436d04919e50f5f9fbafc01889
NikolaySimakov <n.simakoff@gmail.com>
2026-03-16 18:32:03 +0300

    Update BaseRiskEstimation method signature

--- risk_estimation/base_risk_estimation.py
+++ risk_estimation/base_risk_estimation.py
@@ -1,9 +1,12 @@
+from ..house_graph.nodes import BaseNode
+
+
 class BaseRiskEstimation:
     def __init__(self):
         pass
     
-    def estimate(self):
+    def estimate(self, node: BaseNode) -> float:
         '''
         Gives the "risk of failure" of each node (based on load and vulnerability).
         '''
-        pass
+        return 0.0
