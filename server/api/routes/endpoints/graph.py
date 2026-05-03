1c552e688b868187e292bbb1c7ebbd54ef7715ed
NikolaySimakov <n.simakoff@gmail.com>
2026-05-03 21:56:26 +0300

    unused code in the graph router has been commented out

--- server/api/routes/endpoints/graph.py
+++ server/api/routes/endpoints/graph.py
@@ -6,7 +6,7 @@ from typing import Dict, Literal
 from house_graph import HouseFactory, HouseGraphDTO
 from house_graph.samples import House15Factory, House16Factory, House27Factory
 
-import pandas as pd # use pandas for excel files
+# import pandas as pd # use pandas for excel files
 
 from ....schemas import GraphType
 
@@ -34,18 +34,18 @@ async def get_building_graph(graph_type: Literal["House15", "House16", "House27"
 
 # ---------------------------------------- Move to another routes: incidents-file (route), schedule (route) -------
 
-@router.post('/incidents-file/')
-async def post_excel_file(file: UploadFile) -> pd.DataFrame:
-    file_df = pd.read_excel(file)
+# @router.post('/incidents-file/')
+# async def post_excel_file(file: UploadFile) -> pd.DataFrame:
+#     file_df = pd.read_excel(file)
     
-    # pip install python-multipart
-    # TODO: fill this method
+#     # pip install python-multipart
+#     # TODO: fill this method
     
-    return file_df # return pandas dataframe
+#     return file_df # return pandas dataframe
 
 
-@router.get('/schedule/')
-async def get_schedule_excel_file():
-    # when we will generate schedule
-    excel_file = ...
-    return excel_file
+# @router.get('/schedule/')
+# async def get_schedule_excel_file():
+#     # when we will generate schedule
+#     excel_file = ...
+#     return excel_file
