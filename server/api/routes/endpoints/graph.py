489b303429839fdf89fbee338ce56376bffb0b80
NikolaySimakov <n.simakoff@gmail.com>
2026-05-03 16:08:48 +0300

    todo 2 new routes

--- server/api/routes/endpoints/graph.py
+++ server/api/routes/endpoints/graph.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI, UploadFile, File
+
 from loguru import logger
 from typing import Dict, Literal
 
 from house_graph import HouseFactory, HouseGraphDTO
 from house_graph.samples import House15Factory, House16Factory, House27Factory
+
+import pandas as pd # use pandas for excel files
 
 from ....schemas import GraphType
 
@@ -27,3 +30,22 @@ async def get_building_graph(graph_type: Literal["House15", "House16", "House27"
         return None
     house = factory.build()
     return house.to_json()
+
+
+# ---------------------------------------- Move to another routes: incidents-file (route), schedule (route) -------
+
+@router.post('/incidents-file/')
+async def post_excel_file(file: UploadFile) -> pd.DataFrame:
+    file_df = pd.read_excel(file)
+    
+    # pip install python-multipart
+    # TODO: fill this method
+    
+    return file_df # return pandas dataframe
+
+
+@router.get('/schedule/')
+async def get_schedule_excel_file():
+    # when we will generate schedule
+    excel_file = ...
+    return excel_file
