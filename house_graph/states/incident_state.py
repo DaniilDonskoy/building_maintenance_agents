ae885331d74c7add608fabb77c14fbfff401330f
Dimas-karas <dm2001@live.ru>
2026-05-10 18:06:43 +0300

    fix errors

--- house_graph/states/incident_state.py
+++ house_graph/states/incident_state.py
@@ -1,4 +1,3 @@
-from typing import Dict, Union
 from dataclasses import dataclass, field
 
 from loguru import logger
@@ -14,21 +13,26 @@ class IncidentState:
     def set_incident(self, message: str):
         if not self.has_incident and not bool(self.message):
             self.has_incident = True
-            self.message = message
             if self.obj:
                 self.obj.house.incident_count += 1
+                message += " (Total incidents in house {}: {})".format(id(self.obj.house), self.obj.house.incident_count)
+            self.message = message
             logger.info("Set incident: {}".format(message))
             
     
     def update_incident(self, message: str):
         if self.has_incident:
+            if self.obj:
+                message += " (Total incidents in house {}: {})".format(id(self.obj.house), self.obj.house.incident_count)
             self.message = message
             logger.info("Update incident: {}".format(message))
             
     def fix_incident(self):
         if self.has_incident:
             self.has_incident = False
-            self.message = ""
+            message = ""
             if self.obj:
                 self.obj.house.incident_count -= 1
-            logger.info("Fix incident")
+                message += " (Total incidents in house {}: {})".format(id(self.obj.house), self.obj.house.incident_count)
+            self.message = message
+            logger.info("Fix incident{}".format(message))
